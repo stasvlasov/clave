69b8e8921b025611ef4d8c5684e2e86f74a68336
Stas Vlasov <stasvlasov@stasvlasov.ru>
2020-08-07 19:55:50 +0200

    Refactor clave minor mode
    - clave-on state (aka command mode) is now set via set-transient-map
    as xah-fly-keys does
    - add function to initialize keymap with dummy functions for rebinding
    - normalize names
    - split code into functions

--- clave.el
+++ clave.el
@@ -1,127 +1,195 @@
-;; global keys (not sure if I need to rebind ESC)
-  (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+;; define hooks
+(defvar clave-on-hook nil
+  "Hook for `clave-on'")
+(defvar clave-off-hook nil
+  "Hook for `clave-off'")
 
-  ;; hooks
-  (defvar clavis-command-mode-activate-hook nil "Hook for `clavis-command-mode-activate'")
-  (defvar clavis-insert-mode-activate-hook nil "Hook for `clavis-insert-mode-activate'")
+;; define keymaps
+(defvar clave-minor-mode-map (make-sparse-keymap)
+  "Keybinding for `clave' minor mode.")
 
+(defvar clave-map (make-sparse-keymap)
+  "Keybinding for `clave' command mode.")
 
-  ;; keymaps
-  (defvar clavis-key-map (make-sparse-keymap) "Keybinding for `clavis' minor mode.")
-  (defvar clavis-insert-mode-keymap (make-sparse-keymap) "Keybinding for `clavis' command mode.")
-  (defvar clavis-command-mode-keymap (make-sparse-keymap) "Keybinding for `clavis' insert mode.")
+;; (setq clave-map (make-sparse-keymap))
 
-  (defvar clavis-leader-keymap (make-sparse-keymap) "Keybinding for `clavis' leader map.")
+;; define variables
+(defvar clave--off-func '(lambda ())
+  "Function to remove map.")
 
-  ;; Another option just in case
-  (define-key clavis-key-map (kbd "<f7>") clavis-leader-keymap)
-  (define-key clavis-key-map (kbd "<f7>") clavis-leader-keymap)
-  (define-key clavis-key-map (kbd "<f8>") 'clavis-command-mode-activate)
-  (define-key clavis-key-map (kbd "<f9>") 'clavis-insert-mode-activate)
+(defvar clave-on-p nil "Clave state.")
 
-  ;; For system settings
-  (define-key clavis-key-map (kbd "<f13>") clavis-leader-keymap)  ;; set CapsLock to f13
-  (define-key clavis-key-map (kbd "<f14>") 'clavis-command-mode-activate)  ;; set Alt to f14
-  ;; does not work with karabiner on Mac (looks like emacs does not see f14)
+(defvar clave-lighter " λ"  ;; ξѣѢѮ£₽⦾λ
+  "Default mode line lighter for minor mode `clave'")
 
-  ;; Convinient bindings when CapsLock and Alt is not set in the system bindings
-  (define-key clavis-command-mode-keymap (kbd "SPC") 'clavis-insert-mode-activate)
-  (define-key clavis-insert-mode-keymap (kbd "M-SPC") 'clavis-command-mode-activate)
+(defvar clave-map-init-standard-keys
+  '(
+    ;; Numbers are Workman's difficulty of keys. The higher number the worse.
+    ;; https://web.archive.org/web/20170311215135/http://www.workmanlayout.com/blog/wp-content/uploads/2010/10/keyboard_graded1.png
+    ;; Numbers row 
+    "`" "~"
+    "1" "!"
+    "2" "@"
+    "3" "#"
+    "4" "$"
+    "5" "%"
+    "6" "^"
+    "7" "&"
+    "8" "*"
+    "9" "("
+    "0" ")"
+    "-" "_"
+    "=" "+"
+    ;; LEFT HAND 
+    ;; - Top row
+    "q" "Q"  ; 4
+    "w" "W"  ; 2
+    "e" "E"  ; 2
+    "r" "R"  ; 3
+    "t" "T"  ; 4
+    ;; - Home row
+    "a" "A"  ; 1.5
+    "s" "S"  ; 1
+    "d" "D"  ; 1
+    "f" "F"  ; 1
+    "g" "G"  ; 3
+    ;; - Bottom row
+    "z" "Z"  ; 4
+    "x" "X"  ; 4
+    "c" "C"  ; 3
+    "v" "V"  ; 2
+    "b" "B"  ; 5
+    ;; RIGHT HAND
+    ;; - Top row
+    "y" "Y"  ; 5
+    "u" "U"  ; 3
+    "i" "I"  ; 2
+    "o" "O"  ; 2
+    "p" "P"  ; 4
+    "[" "{"  
+    "]" "}"  
+    "\\" "|" 
+    ;; - Home row
+    "h" "H"  ; 3
+    "j" "J"  ; 1
+    "k" "K"  ; 1
+    "l" "L"  ; 1
+    ";" ":"  ; 1.5
+    "'" "\""
+    ;; - Bottom row
+    "n" "N"  ; 3
+    "m" "M"  ; 2
+    "," "<"  ; 3
+    "." ">"  ; 4
+    "/" "?"  ; 4
+    ))
 
-  ;; (define-key clavis-command-mode-keymap (kbd "a") clavis-leader-keymap)
-  ;; (define-key clavis-command-mode-keymap (kbd "a") nil)
+(defvar clave-map-init-standard-extra-keys
+  '("TAB"
+    "RET"
+    "SPC"
+    "DEL"))
 
+;;;###autoload
+(defmacro clave-map-init (map-name &optional extra-keys keys)
+  "Creates MAP-NAME keymap if is does not exist and binds dummy commands to KEYS. If KEYS is nil use `clave-map-init-standard-keys' list of keys instead. If EXTRA-KEYS are set it creates extra dummy functions and binds it to the end of MAP-NAME keymap. If EXTRA-KEYS is set to t it uses `clave-map-init-standard-keys' list as extra keys. Both KEYS and EXTRA-KEYS should be list of valid `kbd' arguments.
+Examples:
+    ;; binds `clave-map-init-standard-keys' to my-map
+    (clave-map-init 'my-map)
+    ;; binds `clave-map-init-standard-keys' and `clave-map-init-standard-extra-keys'
+    (clave-map-init 'my-map t)
+    ;; add one extra key
+    (clave-map-init 'my-map '(\"ESC\"))
+    ;; binds one key
+    (clave-map-init 'my-map nil '(\"ESC\"))"
+  `(progn
+     ;; check keymap
+     (unless (boundp ,(eval map-name))
+       (defvar ,(eval map-name) (make-sparse-keymap)))
+     ;; define dummy functions
+     ,@(mapcar
+        (lambda (key)
+          (let ((func-name
+                 (make-symbol (concat (symbol-name (eval map-name)) "-" key))))
+            `(progn
+               (defun ,func-name ()
+                 "This is clave dummy command meant for rebinding."
+                 (interactive))
+               (define-key ,(eval map-name) (kbd ,key) (quote ,func-name)))))
+        (append (or (eval keys)
+                    clave-map-init-standard-keys)
+                (when extra-keys
+                  (if (equal extra-keys t)
+                      clave-map-init-standard-extra-keys
+                    (eval extra-keys)))))))
 
-  (defun clavis-command-mode-activate ()
-    "Activate command mode."
-    (interactive)
-    (modify-all-frames-parameters (list (cons 'cursor-type 'box)))
-    (global-hl-line-mode 1)  ;; highligh lines when in command mode
-    ;; (setq overriding-local-map clavis-command-mode-keymap)
-    (set-keymap-parent clavis-key-map clavis-command-mode-keymap)
-    (run-hooks 'clavis-command-mode-activate-hook)
-    (setq clavis-insert-state-q nil))
+;; populate clave-map with dummy commands
+(clave-map-init 'clave-map)
 
+;; functions
+(defun clave-on-indicate ()
+  "Indicate clave on state."
+  (modify-all-frames-parameters (list (cons 'cursor-type 'box)))
+  (global-hl-line-mode 1))
 
-  (defun clavis-insert-mode-activate ()
-    "Activate insertion mode."
-    (interactive)
-    (modify-all-frames-parameters (list (cons 'cursor-type 'bar)))
-    (global-hl-line-mode 0)  ;; do not highlight lines it insert mode
-    ;; (setq overriding-local-map nil)
-    (set-keymap-parent clavis-key-map clavis-insert-mode-keymap)
-    (run-hooks 'clavis-insert-mode-activate-hook)
-    (setq clavis-insert-state-q t))
+(defun clave-off-indicate ()
+  "Indicate clave on state."
+  (modify-all-frames-parameters (list (cons 'cursor-type 'bar)))
+  (global-hl-line-mode 0))
 
-
-
-;; when going into minibuffer, switch to insertion mode.
-(add-hook 'minibuffer-setup-hook 'clavis-insert-mode-activate)
-(add-hook 'minibuffer-setup-hook 'clavis-deactivate-input-method t)
-(add-hook 'minibuffer-exit-hook 'clavis-command-mode-activate)
-
-;; when in shell mode, switch to insertion mode.
-(add-hook 'shell-mode-hook 'clavis-deactivate-input-method t)
-(add-hook 'shell-mode-hook 'clavis-insert-mode-activate)
-
-
-
-
-  ;; Preserve input method when switching to command mode
-  (setq clavis-insert-mode-input-method nil)  ;; assume default input method at begining
-  (setq clavis-input-method-lighter " ξ") ;; this is for the minor mode lighter
-
-  (defun clavis-insert-mode-toggle-input-method ()
-    (activate-input-method clavis-insert-mode-input-method))
-
-  (defun clavis-command-mode-preserve-input-method ()
-    (when clavis-insert-state-q
-      (setq clavis-insert-mode-input-method current-input-method))
-    (deactivate-input-method))
-
-
-  (add-hook 'clavis-command-mode-activate-hook  'clavis-command-mode-preserve-input-method)
-  (add-hook 'clavis-insert-mode-activate-hook  'clavis-insert-mode-toggle-input-method)
-
-
-  (defun clavis-toggle-input-method ()
-    (interactive)
-    ;; change to input mode when toggling input method
-    (unless clavis-insert-state-q
-      (clavis-insert-mode-activate))
-    ;; change language spelling and lighter
-    (if current-input-method
-        (clavis-deactivate-input-method)
-      (clavis-activate-input-method)))
-
-(defun clavis-activate-input-method ()
+;;;###autoload
+(defun clave-on ()
+  "Activate `clave' command mode."
   (interactive)
-  (clavis-set-ispell-russian)
-  (activate-input-method default-input-method)
-  (setq current-input-method-title nil)
-  (setq clavis-input-method-lighter #(" ѣ" 1 2  (face org-formula))))
+  (setq clave--off-func
+        (set-transient-map clave-map (lambda () t)))
+  (setq clave-on-p t)
+  (clave-on-indicate)
+  (run-hooks 'clave-on-hook))
 
-(defun clavis-deactivate-input-method ()
+;;;###autoload
+(defun clave-off ()
+  "Activate `clave' insertion mode."
   (interactive)
-  (clavis-set-ispell-english)
-          (deactivate-input-method)
-          (setq current-input-method-title nil)
-          (setq clavis-input-method-lighter #(" ξ" 1 2  (face org-formula))))
+  (funcall clave--off-func)
+  (setq clave-on-p nil)
+  (clave-off-indicate)
+  (run-hooks 'clave-off-hook))
 
+;; we need an escape from clave-on
+(define-key clave-map (kbd "SPC") 'clave-off)
 
+;; clave minor mode
+(defun clave-set-hooks ()
+  "Sets hooks for `clave' minor mode states"
+  (add-hook 'minibuffer-setup-hook 'clave-off)
+  (add-hook 'shell-mode-hook 'clave-)
+  (add-hook 'minibuffer-exit-hook 'clave-command-mode-activate))
 
-  ;; minor-mode-alist
+(defun clave-unset-hooks ()
+  "Unets hooks for `clave' minor mode states. Used for turning `clave' minor mode of."
+  (remove-hook 'minibuffer-setup-hook 'clave-off)
+  (remove-hook 'shell-mode-hook 'clave-)
+  (remove-hook 'minibuffer-exit-hook 'clave-command-mode-activate))
 
-  ;; Define Clavis (minor) Mode
-  (define-minor-mode clavis
-    "A personalized modal keybinding set, like vim, but based on ergonomic principles, like Dvorak layout and personal preferences. Modified version of xah-fly-keys (URL `http://ergoemacs.org/misc/ergoemacs_vi_mode.html')"
-    :init-value nil
-    :global t
-    :lighter (:eval clavis-input-method-lighter) ;; ξѣѢѮ£₽⦾
-    :keymap clavis-key-map
-    (add-to-list 'emulation-mode-map-alists
-                 '((cons 'clavis-keys clavis-key-map)))
-    (clavis-insert-mode-activate))
+;;;###autoload
+(define-minor-mode clave
+  "A personalized modal keybinding set, like vim, but based on ergonomic principles, like Dvorak layout and personal preferences. Inspired by xah-fly-keys (URL `http://ergoemacs.org/misc/ergoemacs_vi_mode.html')"
+  :init-value nil
+  :global t
+  :lighter (:eval clave-lighter) 
+  :keymap clave-minor-mode-map
+  (if clave
+      (progn
+        (clave-off-indicate)
+        (clave-map-init 'clave-map)
+        (clave-set-hooks))
+    (progn
+      (clave-unset-hooks)
+      (clave-off))))
 
-  (provide 'clavis)
-  (clavis t)
+(provide 'clave)
+
+;; (clave t)
+
+;; clave.el ends here
